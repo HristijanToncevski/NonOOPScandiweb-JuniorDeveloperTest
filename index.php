bbd4889db468eebb527e13dcc702efd151f48725
Hristijan Toncevski <58054308+HristijanToncevski@users.noreply.github.com>
2021-12-23 16:48:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-    include_once 'classes/Product.php';
     include_once "Database.php";
 ?>
 <!DOCTYPE html>
@@ -17,17 +16,33 @@
         <div class="header">
             <span>Product List</span>
             <div class="btn-container">
-                <button onclick="window.location='ProductAdd.php'">ADD</button>
-                <button id="delete-product-btn">MASS DELETE</button>
+                <button type="submit" name="add_button" onclick="document.location='ProductAdd.php'">ADD</button>
+                <button form="form1" id="delete-product-btn" type="submit" name="delete_button" value="MASS DELETE">MASS DELETE</button>
             </div>
             <hr>
-        </div>
+        </div>  
     </div>
-
-    <div id="product-list">
-        <?php
-            
-        ?>
+    <form action="delete_product.php" method="post" id="form1">
+    <div id="product-list">          
+            <?php
+                $query = "SELECT * FROM products";
+                $result_products = mysqli_query($connection, $query);
+                while($row = mysqli_fetch_array($result_products)) {
+            ?> 
+                <div class="container">
+                        <input type="checkbox" class="delete-checkbox" type="checkbox" name="delete[]" value="<?php echo $row['id']; ?>">
+                        <p><?php echo $row['sku']; ?></p>
+                        <p><?php echo $row['name']; ?></p>
+                        <p><?php echo $row['price']; ?>.00 $</p>
+                        <?php if ($row['productType'] == 'DVD'): ?>
+                        <p>Size: <?php echo $row['size']; ?> MB</p>
+                        <?php elseif ($row['productType'] == 'Book'): ?>
+                        <p>Weight: <?php echo $row['weight']; ?> KG</p>
+                        <?php elseif ($row['productType'] == 'Furniture'): ?>
+                        <p> Dimensions: <?php echo $row['height']; ?>x<?php echo $row['width']; ?>x<?php echo $row['length']; ?></p>  
+                        <?php endif ?>
+                </div>
+            <?php } ?>        
     </div>
     
     <div class="footer-div">
@@ -36,5 +51,6 @@
             <p>Scandiweb Test assignment</p>
         </div>
     </div>
+</form>
 </body>
 </html>
