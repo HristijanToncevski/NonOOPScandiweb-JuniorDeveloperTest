a1098a0da7aa58d9dbccee4e33f596168da5a44c
Hristijan Toncevski <58054308+HristijanToncevski@users.noreply.github.com>
2021-12-23 16:45:52 +0100

    Update delete_product.php

--- delete_product.php
+++ delete_product.php
@@ -1,3 +1,16 @@
 <?php
 include_once "Database.php";
+
+if(isset($_POST['delete_button'])){
+    if(!empty($_POST['delete'])){
+    $all = $_POST['delete'];
+    $string = implode(",", $all);
+    $query = "DELETE FROM products WHERE id IN ($string)";
+    $result = mysqli_query($connection, $query);
+    header("Location: index.php");
+    exit;
+}
+header("Location: index.php");
+exit;
+}
 ?>
